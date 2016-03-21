44479e85952308f2afbd97d02877e59e2b4449ea
Wooden-Robot <wangtianxing1993@126.com>
2016-03-21 09:10:28 +0800

    百度新闻爬虫2.0
    1.修改了获取信息的方式；
    2.添加所有网页爬取功能。

---
+++ 百度新闻爬虫/baidunews2.0.py
@@ -0,0 +1,71 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+
+download_url = 'http://news.baidu.com/ns?word=%E7%99%BE%E5%BA%A6'
+final_contents = {}
+n = 1
+
+
+def word():
+    global x
+    x = input('请输入搜索关键词：')
+    payload = {'word': x}
+    return payload
+
+
+# html下载器
+def html_downloader(url, payload):
+    res = requests.get(url, params=payload)
+    print('Downloading', res.url)
+    return res.content
+
+
+# html解析器
+def html_parser(html):
+    global n
+    contents = {}
+    soup = BeautifulSoup(html, 'lxml')
+    content_list = soup.find_all('div', class_='result')
+    for content in content_list:
+        # 获取标题
+        title = content.find('a', target='_blank').stripped_strings
+        title = ''.join(title)
+        # 获取来源和时间：
+        time_from = content.find('p', class_="c-author").string
+        type(time_from)
+        time_from = time_from.replace('\xa0\xa0', ', ')
+        # 获取摘要：
+        content.find('p', 'c-author').extract()
+        content.find('span', 'c-info').extract()
+        content.find('h3', 'c-title').extract()
+        summary = ''.join(content.stripped_strings)
+        # 储存数据：
+        contents[n] = {}
+        contents[n]['title'] = title
+        contents[n]['time_from'] = time_from
+        contents[n]['sumary'] = summary
+        n += 1
+    next_page = soup.find('strong').next_sibling
+    if next_page:
+        return contents, download_url[0:22] + next_page['href']
+    else:
+        return contents, None
+
+
+# 调度中心
+def spider_main():
+    global final_contents
+    url = download_url
+    payload = word()
+    while url:
+        html = html_downloader(url, payload)
+        contents, url = html_parser(html)
+        final_contents.update(contents)
+    final_contents = json.dumps(final_contents, ensure_ascii=False)
+    print(final_contents)
+
+
+if __name__ == '__main__':
+    spider_main()
