f55684e8f4478eb3fb665a4d64198a374c0164e0
Wooden-Robot <wangtianxing1993@126.com>
2016-05-20 13:29:52 +0800

    增加爬取失败后自动重新爬取当前网址

--- 拉勾网爬虫/lagou2.0.py
+++ 拉勾网爬虫/lagou2.0.py
@@ -5,12 +5,16 @@ __author__ = 'woodenrobot'
 import requests
 import json
 import datetime
+import time
 from pymongo import MongoClient
 
 
 n = 1
-login_data = {'first': 'true', 'pn': str(n), 'kd': 'python'}
+login_data = {'first': 'true', 'pn': str(n), 'kd': 'iOS'}
 MONGO_CONN = MongoClient('localhost', 27017)
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 '
+#                          '(KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
+
 
 def download_page(data):
     global n, login_data
@@ -19,16 +23,24 @@ def download_page(data):
         sth = 'true'
     else:
         sth = 'false'
-    login_data = {'first': sth, 'pn': str(n), 'kd': 'python'}
-    data = requests.post('http://www.lagou.com/jobs/positionAjax.json?', data=data).text
+    login_data = {'first': sth, 'pn': str(n), 'kd': ''}
+    try:
+        data = requests.post('http://www.lagou.com/jobs/positionAjax.json?px=new&needAddtionalResult=false',
+                             data=data, timeout=2).text
+    except:
+        print('*******************第{0}页失败,重新爬取*******************'.format(n))
+        data = requests.post('http://www.lagou.com/jobs/positionAjax.json?px=new&needAddtionalResult=false',
+                             data=data).text
+    print(data)
     n += 1
     json_obj = json.loads(data)
-    s = json_obj['content']['result'][1:17]
+    s = json_obj['content']['positionResult']['result']
     return s
 
 
 def save(datas):
     for data in datas:
+        data['updataTime'] = datetime.datetime.now()
         MONGO_CONN['database']['lagou'].update_one(
         filter={'_id': data["positionId"]},
         update={'$set': data},
@@ -40,7 +52,9 @@ def main():
     while n:
         s = download_page(login_data)
         if s == []:
+            print('*******************爬取完毕*******************')
             break
+        print(s)
         save(s)
 
 if __name__ == '__main__':
