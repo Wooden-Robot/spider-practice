1449b3383458704d61b58e1556b55ec860f92bc4
Wooden-Robot <wangtianxing1993@126.com>
2016-05-25 15:14:48 +0800

    改善为使用代理Ip爬取拉勾技术类招聘信息

--- 拉勾网爬虫/lagou2.0.py
+++ 拉勾网爬虫/lagou2.0.py
@@ -6,42 +6,52 @@ import requests
 import json
 import datetime
 import time
+import random
 from pymongo import MongoClient
+from lagou_job_list import spider_manager
 
 
 n = 1
+job = ''
+s = ["http://111.206.190.155:80",
+     "http://58.252.2.5:8003"]
+ip = random.choice(s)
+proxies = {"http": ip}
 MONGO_CONN = MongoClient('localhost', 27017)
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 '
-#                          '(KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
 
 
 def download_page():
-    global n
-    print('*******************正在爬取第{0}页******************'.format(n))
+    global n, job
+    print('*******************正在爬取'+job+'第{0}页******************'.format(n))
     if n == 1:
         sth = 'true'
     else:
         sth = 'false'
-    login_data = {'first': sth, 'pn': str(n), 'kd': 'Python'}
+    login_data = {'first': sth, 'pn': str(n), 'kd': job}
     try:
-        res = requests.post('http://www.lagou.com/jobs/positionAjax.json?px=new&needAddtionalResult=false',
-                             data=login_data, timeout=2).text
+        print('代理ip为:', proxies)
+        res = requests.post('http://www.lagou.com/jobs/positionAjax.json?px='
+                            'new&needAddtionalResult=false',
+                             data=login_data, proxies=proxies).text
+        time.sleep(0.3)
     except:
-        print('*******************第{0}页失败,重新爬取*******************'.format(n))
-        res = requests.post('http://www.lagou.com/jobs/positionAjax.json?px=new&needAddtionalResult=false',
-                             data=login_data).text
-    print(res)
+        print('*******************第{0}页失败,重新爬取*******************'
+              .format(n), '/n', proxies)
+        res = requests.post('http://www.lagou.com/jobs/positionAjax.json?px='
+                            'new&needAddtionalResult=false',
+                             data=login_data, proxies=proxies).text
+    # print(res)
     n += 1
     json_obj = json.loads(res)
-    s = json_obj['content']['positionResult']['result']
-    return s
+    datas = json_obj['content']['positionResult']['result']
+    return datas
 
 
 def save(datas):
     for data in datas:
-        print(data['positionId'])
+        # print(data['positionId'])
         data['updataTime'] = datetime.datetime.now()
-        MONGO_CONN['database']['lagou_Python'].update_one(
+        MONGO_CONN['database']['lagou2'].update_one(
         filter={'_id': data["positionId"]},
         update={'$set': data},
         upsert=True
@@ -49,13 +59,19 @@ def save(datas):
 
 
 def main():
-    while n:
-        s = download_page()
-        if s == []:
-            print('*******************爬取完毕*******************')
-            break
-        print(s)
-        save(s)
+    global job, n
+    job_list = spider_manager()
+    print(job_list)
+    for job in job_list:
+        job = job
+        n = 1
+        while n:
+            s = download_page()
+            if s == []:
+                print('*******************'+job+'爬取完毕*******************')
+                break
+            print(s)
+            save(s)
 
 if __name__ == '__main__':
     main()
